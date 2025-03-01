c8c26f4cb567580107d24d425fff792f65319cc2
“shubhamchoudhary-2003” <shubhamera484@gmail.com>
2025-03-01 18:29:32 +0530

    added soem chages

---
+++ components/CustomMDXComponents.ts
@@ -0,0 +1,17 @@
+const path = require('path');
+
+module.exports = withNextra({
+  reactStrictMode: true,
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@medusajs/docs': path.join(__dirname, 'components/docs/CustomMDXComponents'),
+      '@theme/CodeBlock': path.join(__dirname, 'components/docs/CustomMDXComponents'),
+    };
+    return config;
+  },
+});
+function withNextra(arg0: { reactStrictMode: boolean; webpack: (config: any) => any; }): any {
+    throw new Error("Function not implemented.");
+}
+
